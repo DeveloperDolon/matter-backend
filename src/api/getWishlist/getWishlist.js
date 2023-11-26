e63f18e3e1955500704c5e82fbc9cbeec441ade2
Dolon Roy <dolonr718@gmail.com>
2023-11-27 02:57:07 +0600

    wishlist properties data access from db api commit

---
+++ src/api/getWishlist/getWishlist.js
@@ -0,0 +1,29 @@
+const checkingJWT = require("../../utils/checkingJWT");
+const wishlistModel = require("../../models/wishlistModel");
+const propertyModel = require("../../models/property");
+
+const getWishlist = async (req, res, next) => {
+    try {
+
+        const isValid = checkingJWT(req.user.user, req.query.email);
+
+        if(!isValid) {
+            return res.status(401).send({ message: "Forbidden Request."});
+        }
+          
+
+        const query = {buyer_email: req.query.email};
+
+        const wishlistData = await wishlistModel.find(query);
+
+        const propertiesId = wishlistData?.map(item => item.property_id);
+
+        const properties = await propertyModel.find({ _id: { $in: propertiesId } });
+
+        res.send({wishlistProperties: properties});
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = getWishlist;
--- src/routes/wishlist/index.js
+++ src/routes/wishlist/index.js
@@ -1,8 +1,11 @@
 const verifyToken = require("../../middlewares/verifyToken");
 const addWishlist = require("../../api/addWishlist/addWishlist");
+const getWishlist = require("../../api/getWishlist/getWishlist");
 const express = require('express');
 const router = express.Router();
 
 router.post("/users-wishlist", verifyToken, addWishlist);
+
+router.get("/users-wishlist", verifyToken, getWishlist);
 
 module.exports = router;
